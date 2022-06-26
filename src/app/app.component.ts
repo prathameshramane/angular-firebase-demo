11d84ff9ba1fe77bcece060565c8ac2c6ddf5378
Prathamesh Ramane <ppramane@gmail.com>
2022-06-26 14:19:32 +0530

    Adding Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -8,11 +8,26 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  coursesDb$: AngularFireList<any>;
   courses$: Observable<any[]>;
   author$: Observable<any>;
 
   constructor(db: AngularFireDatabase) {
-    this.courses$ = db.list("/courses").valueChanges();
+    this.coursesDb$ = db.list("/courses");
+    this.courses$ = this.coursesDb$.valueChanges();
     this.author$ = db.object("/authors/1").valueChanges();
+  }
+
+  add(course: HTMLInputElement) {
+    this.coursesDb$.push({
+      title: course.value,
+      price: 1500,
+      isLive: true,
+      sections: [
+        { title: "Angular Fundamentals" },
+        { title: "Angular Advance" },
+        { title: "Angular Firebase" },
+      ]
+    })
   }
 }
