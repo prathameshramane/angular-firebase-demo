2038f76e66cf89507960df55b2c9ac4e54cfb2e2
Prathamesh Ramane <ppramane@gmail.com>
2022-06-26 13:42:43 +0530

    Managing Memory Leaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   courses: any;
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-    db.list("/courses").valueChanges().subscribe(courses => {
+    this.subscription = db.list("/courses").valueChanges().subscribe(courses => {
       this.courses = courses;
       console.log(this.courses);
     })
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
